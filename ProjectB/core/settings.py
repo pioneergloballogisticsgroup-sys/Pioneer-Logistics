015d52f24ae4288324069e74fabb388f9648ea03
pioneergloballogisticsgroup-sys <pioneergloballogisticsgroup@gmail.com>
2025-11-24 18:01:38 +0100

    FINAL FIX: Corrected INSTALLED_APPS reference for 'website' app.

--- ProjectB/core/settings.py
+++ ProjectB/core/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
+    'ProjectB.website',  # <--- THIS IS THE FIX
 ]
 
 MIDDLEWARE = [
